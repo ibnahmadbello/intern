555d11703ecfc5d34408bf0b2653d5a60ef877e3
ibnahmadbello <justforlearningcode@gmail.com>
2023-03-31 22:37:29 +0100

    Returning map as response

--- src/main/java/com/arab/intern/controllers/CallController.java
+++ src/main/java/com/arab/intern/controllers/CallController.java
@@ -1,7 +1,9 @@
 package com.arab.intern.controllers;
 
 import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,13 +16,33 @@ public class CallController {
 	@GetMapping(value = "/getData")
 	public ResponseEntity<?> getData(@RequestParam String email, 
 			@RequestParam String password) {
-		return ResponseEntity.ok(new HashMap<>());
+		ResponseEntity<?> response = null;
+		try {
+			if(email.isEmpty() || password.isEmpty()) {
+				throw new RuntimeException("One of the parameters is not valid");
+			}
+			response = new ResponseEntity<>(new HashMap<>(), HttpStatus.OK);
+		}
+		catch (Exception e) {
+			response = new ResponseEntity<String>("This is a server error", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return response;
 	}
 	
 	@PostMapping(value = "/postData")
 	public ResponseEntity<?> postData(@RequestParam String email, 
 			@RequestParam String password) {
-		return ResponseEntity.ok(new HashMap<>());
+		ResponseEntity<?> response = null;
+		try {
+			if(email.isEmpty() || password.isEmpty()) {
+				throw new RuntimeException("One of the parameters is not valid");
+			}
+			response = new ResponseEntity<>(new HashMap<>(), HttpStatus.OK);
+		}
+		catch (Exception e) {
+			response = new ResponseEntity<String>("This is a server error", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return response;
 	}
 	
 }
